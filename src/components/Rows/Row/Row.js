2f80c3796ce60c9148aad9c45a44eb10262a133f
Tariku Bewket <tarikubew@gmail.com>
2024-03-27 13:04:26 +0100

    row list added

---
+++ src/components/Rows/Row/Row.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import './row.css';
+import axios from '../../../utils/axios';
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube';
+
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
+
+  const base_url = 'https://image.tmdb.org/t/p/original';
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(fetchUrl);
+        setMovies(request.data.results);
+      } catch (error) {
+        console.log('error', error);
+      }
+    })();
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+      origin: 'http://localhost:3000',
+    },
+  };
+
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row__posters">
+        {movies?.map((movie, index) => (
+          <img
+            onClick={() => handleClick(movie)}
+            key={index}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+          />
+        ))}
+      </div>
+      <div style={{ padding: '40px' }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
